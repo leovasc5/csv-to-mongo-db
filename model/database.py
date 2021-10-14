84af89894faa78c2422e807f208705c2d7969b47
leovasc5 <leovasc5@hotmail.com>
2021-10-14 18:56:51 -0300

    Developed find function

--- model/database.py
+++ model/database.py
@@ -1,5 +1,8 @@
+from json.encoder import JSONEncoder
 import os, time, json
 from pymongo import MongoClient
+from pprint import pprint
+
 from model.planilha import transferirDados
 
 def createBase():
@@ -30,69 +33,78 @@ def consulta():
     nomeHost = input('Nome do host: ')
     porta = int(input('Porta do host: '))
 
-    # try:
-    if 1 == 1:
-        client = MongoClient(nomeHost, porta)
-        print('Procurando...\nAguarde, isso pode demorar um pouco...')
-        os.system('cls')
-        print('\Escolha uma das bases de dados encontradas: ')
-        bases = client.list_database_names()
-        n = 0
+    try:
+        if 1 == 1:
+            client = MongoClient(nomeHost, porta)
+            print('Procurando...\nAguarde, isso pode demorar um pouco...')
+            os.system('cls')
+            print('Escolha uma das bases de dados encontradas: ')
+            bases = client.list_database_names()
+            n = 0
 
-        for i in bases:
-            print(str(n+1)+ ' - ' + i)
-            n+=1
+            for i in bases:
+                print(str(n+1)+ ' - ' + i)
+                n+=1
 
-        try:
-            key = int(input('\nUser: '))
-        except:
-            print('\nErro, entrada inválida\nTente novamente...')
-            time.sleep(2)
-            consulta()
+            try:
+                key = int(input('\nUser: '))
+            except:
+                print('\nErro, entrada inválida\nTente novamente...')
+                time.sleep(2)
+                consulta()
 
-        if (key <= 0) or (key > (len(bases)+1)):
-            print('\nErro, entrada inválida\nTente novamente...')
-            time.sleep(2)
-            consulta()
+            if (key <= 0) or (key > (len(bases)+1)):
+                print('\nErro, entrada inválida\nTente novamente...')
+                time.sleep(2)
+                consulta()
 
-        os.system('cls')
-        colecoes = client[bases[key-1]].collection_names()
-        n = 0
+            os.system('cls')
+            colecoes = client[bases[key-1]].collection_names()
+            n = 0
 
-        for i in colecoes:
-            print(str(n+1) + ' - ' + i + '\n')
-            n+=1
+            for i in colecoes:
+                print(str(n+1) + ' - ' + i + '\n')
+                n+=1
 
-        try:
-            key2 = int(input('Escolha a coleção: '))
-        except:
-            print('\nErro, entrada inválida\nTente novamente...')
-            time.sleep(2)
-            consulta()
+            try:
+                key2 = int(input('Escolha a coleção: '))
+            except:
+                print('\nErro, entrada inválida\nTente novamente...')
+                time.sleep(2)
+                consulta()
 
-        if (key2 <= 0) or (key2 > (len(colecoes)+1)):
-            print('\nErro, entrada inválida\nTente novamente...')
-            time.sleep(2)
-            consulta()
-        
-        print('Base escolhida: ' + colecoes[key2-1])
-        query = input('\nQuery: ')
+            if (key2 <= 0) or (key2 > (len(colecoes)+1)):
+                print('\nErro, entrada inválida\nTente novamente...')
+                time.sleep(2)
+                consulta()
 
-        print(json.dumps(query))
-        # try:
-        # print(client[bases[key-1]][colecoes[key2-1]].find(query).pretty())
-        # client[bases[key-1]].colecoes[key2-1].find(json.dumps(query))
-        # except:
-        # os.system('cls')
-        # print('Não foi possível realizar a query :(')
-        # print('Você será direcionado para a tela principal')
-        # time.sleep(3)
+            print('Base escolhida: ' + colecoes[key2-1])
+            print(client[bases[key-1]][colecoes[key2-1]])
+            query = input('\nQuery: ')
 
-    # except:
-        # print('\nNenhuma base encontrada\nTente novamente...')
-        # time.sleep(2)
-        # consulta()
+            try:
+                res = client[bases[key-1]][colecoes[key2-1]].find(json.loads(query))
+                os.system('cls')
+                for i in res:
+                    pprint(i)
 
+                again = int(input('\n1 - Fazer mais uma consulta \n2 - Encerrar o processo \n\nUser:'))
+                if again == 1:
+                    consulta()
+                else:
+                    os.system('cls')
+                    print('Volte sempre! :)')
+                    time.sleep(3)
+                    exit()
 
-    
+            except:
+                os.system('cls')
+                print('Não foi possível realizar a query :(')
+                print('\nTente novamente...')
+                time.sleep(3)
+                consulta()
 
+    except:
+        print('\nNenhuma base encontrada\nTente novamente...')
+        time.sleep(2)
+        consulta()
